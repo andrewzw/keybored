a38ae742d1d86fa236654aa1d0891955ee4d2763
andrewzw <yapzhewei2002@gmail.com>
2023-04-28 01:21:37 +1000

    updated

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import Header from './components/Header/Header';
 import TypingArea from './components/TypingArea/TypingArea';
 import Stats from './components/Stats/Stats';
 import './App.css';
+
 function App() {
-  const [textToType, setTextToType] = useState('');
-
-  const sampleTexts = [
-    'The quick brown fox jumps over the lazy dog.',
-    'To be or not to be, that is the question.',
-    'The only thing we have to fear is fear itself.',
-    // Add more sample texts here
-  ];
-
-  useEffect(() => {
-    const newText = sampleTexts[Math.floor(Math.random() * sampleTexts.length)];
-    setTextToType(newText);
-  }, []);
   return (
     <div className="App">
       <Header />
-      <TypingArea
-        textToType={textToType}
-      />
-      <Stats
-      />
+      <TypingArea />
+      <Stats />
     </div>
   );
 }
--- src/components/TypingArea/TypingArea.js
+++ src/components/TypingArea/TypingArea.js
@@ -1,21 +1,72 @@
-function TypingArea({ textToType, userInput, onUserInputChange, onKeyDown }) {
-    const handleChange = (event) => {
-      onUserInputChange(event);
-    };
-  
-    return (
-      <div className="typing-area">
-        <h2>{textToType}</h2>
-        <input
-          type="text"
-          value={userInput}
-          onChange={handleChange}
-          onKeyDown={onKeyDown}
-          placeholder="Start typing here..."
-        />
-      </div>
-    );
-  }
-  
-  export default TypingArea;
-  
+import React, { useState, useEffect } from 'react';
+import './typing_area.css';
+
+function TypingArea() {
+  const [textToType, setTextToType] = useState('');
+  const [userInput, setUserInput] = useState('');
+
+  const sampleTexts = [
+    'The quick brown fox jumps over the lazy dog.',
+    'To be or not to be, that is the question.',
+    'The only thing we have to fear is fear itself.',
+    // Add more sample texts here
+  ];
+
+  const handleUserInputChange = (event) => {
+    setUserInput(event.target.value);
+  };
+
+  const compareText = (textToType, userInput) => {
+    const textToTypeArray = textToType.split(' ');
+    const userInputArray = userInput.split(' ');
+    let matchCount = 0;
+
+    for (let i = 0; i < textToTypeArray.length; i++) {
+      if (textToTypeArray[i] === userInputArray[i]) {
+        matchCount++;
+      }
+    }
+
+    return matchCount === textToTypeArray.length;
+  };
+
+  const findMismatchIndex = (textToType, userInput) => {
+    const textToTypeArray = textToType.split(' ');
+    const userInputArray = userInput.split(' ');
+
+    for (let i = 0; i < textToTypeArray.length; i++) {
+      if (textToTypeArray[i] !== userInputArray[i]) {
+        return textToTypeArray[i]; //return mismatched word
+      }
+    }
+    return true; //return true if no mismatch
+  };
+
+
+  useEffect(() => {
+    const newText = sampleTexts[Math.floor(Math.random() * sampleTexts.length)];
+    setTextToType(newText);
+  }, []);
+
+  const mismatchIndex = findMismatchIndex(textToType, userInput);
+
+  return (
+    <div className="typing-area">
+      <h2>{textToType}</h2>
+      <h3>{compareText(textToType, userInput) ? '' : 'No match!'}
+        {mismatchIndex === true
+          ? ' Match!'
+          : ` Missed word: '${mismatchIndex}`
+        }'
+      </h3>
+      <input
+        type="text"
+        value={userInput}
+        onChange={handleUserInputChange}
+        placeholder="Start typing here..."
+      />
+    </div>
+  );
+}
+
+export default TypingArea;
