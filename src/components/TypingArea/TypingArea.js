f111ddb9136b79d35bb6c98903584835e2fd05bd
andrewzw <yapzhewei2002@gmail.com>
2023-04-29 14:03:14 +1000

    added keyboard functionality

--- src/components/TypingArea/TypingArea.js
+++ src/components/TypingArea/TypingArea.js
@@ -4,7 +4,7 @@ import './typing_area.css';
 function TypingArea() {
   const [textToType, setTextToType] = useState('');
   const [userInput, setUserInput] = useState('');
-  const [typed, setTyped] = useState([]);
+  const [typed, setTyped] = useState('');
 
   const sampleTexts = [
     'The quick brown fox jumps over the lazy dog.',
@@ -27,47 +27,68 @@ function TypingArea() {
       if (textToTypeArray[i] === userInputArray[i]) {
         matchCount++;
       }
-    }
-
-    return matchCount === textToTypeArray.length;
-  };
-
-  const findMismatchIndex = (textToType, userInput) => {
-    const textToTypeArray = textToType.split(' ');
-    const userInputArray = userInput.split(' ');
-
-    for (let i = 0; i < textToTypeArray.length; i++) {
-      if (textToTypeArray[i] !== userInputArray[i]) {
+      else if(textToTypeArray[i] !== userInputArray[i]){
         return textToTypeArray[i]; //return mismatched word
       }
     }
-    return true; //return true if no mismatch
+
+    return matchCount === textToTypeArray.length;//return true if no mismatch
   };
 
 
 
+  const renderUserInput = () => {
+    const textToTypeArray = textToType.split('');
+    const userInputArray = userInput.split('');
 
-  document.addEventListener("keydown", (event) => {
-    let pressed = event.key;
-    if (pressed.length == 1 && pressed.match(/[a-zA-z0-9]/i)) 
-      setUserInput(pressed);
-      typed.push(pressed);
-      console.log(typed);
+    return userInputArray.map((word, index) => {
+      const isMatch = textToTypeArray[index] === word;
+      const wordStyle = {
+        color: isMatch ? 'green' : 'red',
+      };
+      return <span key={index} style={wordStyle}>{word}</span>;
+    });
+  };
 
-});
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      let pressed = event.key;
+  
+      setUserInput((prevInput) => {
+        let newInput = prevInput;
+  
+        if (pressed.length === 1 && pressed.match(/[a-zA-Z0-9,.]/i)) {
+          newInput = prevInput + pressed;
+        } else if (pressed === " " || pressed === "Spacebar") {
+          newInput = prevInput + " ";
+        } else if (pressed === "Backspace") {
+          if (event.ctrlKey) {
+            const lastSpaceIndex = prevInput.lastIndexOf(" ");
+            newInput = lastSpaceIndex >= 0 ? prevInput.slice(0, lastSpaceIndex) : "";
+          } else {
+            newInput = prevInput.slice(0, -1);
+          }
+        }
+  
+        console.log(newInput);
+        return newInput;
+      });
+    };
+  
+    document.addEventListener("keydown", handleKeyPress);
+  
+    // Cleanup: Remove event listener when the component unmounts
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
 
-  const mismatchIndex = findMismatchIndex(textToType, userInput);
+  const mismatchIndex = compareText(textToType, userInput);
 
   return (
     <div className="typing-area">
       <h2>{textToType}</h2>
-      <h3>{compareText(textToType, userInput) ? '' : 'No match!'}
-        {mismatchIndex === true
-          ? ` Match! `
-          : ` Missed word: ${mismatchIndex}`
-        }'
-      </h3>
-      <h2>{userInput}</h2>
+      <h2 className='user-input'>{renderUserInput()}</h2>
       <p>{typed}</p>
     </div>
   );
